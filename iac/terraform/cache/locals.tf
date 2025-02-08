78c3e72f8d21f7fef25b55c41316cc78b90dfc40
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2025-02-08 10:38:19 -0300

    Feat/refactor terraform files (#2)
    * Refactor terraform files
    
    * Refactor folder and add action

--- iac/terraform/cache/locals.tf
+++ iac/terraform/cache/locals.tf
@@ -2,10 +2,14 @@ locals {
   region = "us-east-1"
   name   = "ex-${basename(path.cwd)}"
 
-  vpc_cidr = "172.31.0.0/16"
+  vpc_cidr = data.terraform_remote_state.network.outputs.vpc_cidr_block
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
     Name = local.name
   }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
 }
--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  default  = "us-east-1"
-  type     = string
-  nullable = false
-}
-
-variable "vpcCidr" {
-  default = "172.31.0.0/16"
-}
